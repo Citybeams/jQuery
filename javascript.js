7d6f7fc84c2500625688ce3bf911b504bde1b3ab
citybeams <nicklohr1@gmail.com>
2014-06-06 15:53:58 -0400

    DOM

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+// changes the h1 node's text
+$(document).ready(function(){
+   $("h1").text("Here I am!");
+
+   // changes all li's to Orlando
+   $("li").text("Orlando");
+
+   //changes element with class of .promo
+   $(".promo").text("Changed!");
+ });
