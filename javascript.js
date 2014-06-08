61cceddb88db5dd8683f2e8cfae18ac1d43a6b6b
citybeams <nicklohr1@gmail.com>
2014-06-08 14:58:22 -0400

    on click

--- javascript.js
+++ javascript.js
@@ -8,4 +8,36 @@ $(document).ready(function(){
 
    //changes element with class of .promo
    $(".promo").text("Changed!");
- });
+
+   //descendant selector, selects all li children of destinations
+   $("#destinations li").text("hi!!");
+
+   //selects only the direct descendants! this symbol >
+   $("#destinations > li").text("direct child only!");
+
+   //selects multiple items, sep by comma
+   $(".promo, #france").text("OVERRIDE");
+
+
+   //selecting elements by traversing the DOM (FASTER!!)
+   $("#destinations").find("li").text("change text!");
+
+   //changes last element's text
+   $("li").last().text("change last element");
+
+
+
+   //puts this created price node at the bottom of #destinations
+   // every time the button class is clicked it adds the new node!!
+   //other useful method is .prepend
+
+    $('button').on('click', function(){
+      var price = $('<p>From $400</p>');
+      $('#destinations').append(price)
+      //line below will append the <p> with the price to the closest node to #destinations
+      $(this).closest('#destinations').append(price);
+      // this line below means that ONLY the button that was clicked will be removed
+      $(this).remove();
+      });
+
+  });
